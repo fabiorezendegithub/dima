36e4a0fcdf03237ced9fc8369dce5512b649d2f8
Fabio Rezende <fabio.rezende@gmail.com>
2024-08-12 19:08:09 -0300

    Identity no blazor
    - Authentication State Provider
    - Cookie Authentication State Provider
    - Claims

---
+++ Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -0,0 +1,50 @@
+﻿using Dima.Core.Models.Account;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+using System.Security.Claims;
+
+namespace Dima.Web.Security;
+
+public class CookieAuthenticationStateProvider(IHttpClientFactory clientFactory) : 
+             AuthenticationStateProvider, 
+             ICookieAuthenticationStateProvider
+{
+    private readonly HttpClient _client = clientFactory.CreateClient(Configuration.HttpClientName);
+    private bool _isAuthenticated = false;
+    public async Task<bool> CheckAuthenticationAsync()
+    {
+        await GetAuthenticationStateAsync();
+        return _isAuthenticated;
+    }
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NotifyAuthenticationStateChanged()
+        => NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+
+    private async Task<User?> GetUser()
+    {
+        try
+        {
+            return await _client.GetFromJsonAsync<User?>("v1/identity/manage/info");
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    private async Task<List<Claim>> GetClaims(User user)
+    {
+        var claims = new List<Claim>()
+        {
+            new(ClaimTypes.Name, user.Email),
+            new(ClaimTypes.Email, user.Email)
+        };
+
+        return claims;
+    }
+}
