359008258f6c8ba2810223f077dd4a9122f0972b
Fabio Rezende <fabio.rezende@gmail.com>
2024-08-20 15:07:19 -0300

    Identity no blazor
    - Inject
    - Resolvendo Dependencias
    - Register Code Behind

--- Dima.Web/Pages/Identity/Register.razor.cs
+++ Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,7 +1,63 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Accounts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Identity;
 
 public partial class RegisterPage : ComponentBase
 {
+    #region Dependences
+    [Inject]
+    public IAccountHandler Handler { get; set; } = null!;
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+    [Inject]
+    public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+    #endregion
+
+    #region Properties
+    public RegisterRequest InputModel { get; set; } = new();
+    public bool IsBusy { get; set; } = false;
+    #endregion
+
+    #region Override
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity is { IsAuthenticated: true })
+            NavigationManager.NavigateTo("/");
+    }
+    #endregion
+
+    public async Task OnValidSubmitAsync()
+    {
+        IsBusy = true;
+        try
+        {
+            var result = await Handler.RegisterASync(InputModel);
+
+            if(result.IsSuccess)
+            {
+                NavigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                Snackbar.Add(result.Message, Severity.Error);
+            }
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -4,6 +4,8 @@ using Dima.Web;
 using MudBlazor.Services;
 using Dima.Web.Security;
 using Microsoft.AspNetCore.Components.Authorization;
+using Dima.Core.Handlers;
+using Dima.Web.Handlers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -23,6 +25,8 @@ builder.Services.AddHttpClient(Configuration.HttpClientName, opt =>
 {
     opt.BaseAddress = new Uri(Configuration.BackendUrl);
 }).AddHttpMessageHandler<CookieHandler>();
+
+builder.Services.AddTransient<IAccountHandler, AccountHandler>();
 
 
 
