685b7b515c6b00a2abaf2fabde29d267aad04689
Fabio Rezende <fabio.rezende@gmail.com>
2024-08-29 17:07:45 -0300

    CRUD: Mudselect de Categorias
    - Agora criamos o select de categorias
    - Nessa parte tivemos que criar um metodo override no cs para preencher
      a lista de categorias
    - No front tivemos que fazer um foreach para montar a lista de
      categorias
    - Infelizmente, existe um bug que quando carrega aparece o id ao inves
      do titulo, talvez em futuras atualizações esse bug seja resolvido. De
      qualquer forma, uma vez selecionado um item, tudo funciona
      corretamente.

--- Dima.Web/Pages/Transactions/Create.razor.cs
+++ Dima.Web/Pages/Transactions/Create.razor.cs
@@ -1,4 +1,6 @@
 ﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
 using Dima.Core.Requests.Transactions;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
@@ -10,15 +12,46 @@ public partial class CreateTransactionPage : ComponentBase
     #region Properties
     public bool IsBusy { get; set; } = false;
     public CreateTransactionRequest InputModel { get; set; } = new();
+    public List<Category> Categories { get; set; } = [];
     #endregion
 
     #region Services
     [Inject]
-    public ITransactionHandler Handler { get; set; } = null!;
+    public ITransactionHandler TransactionHandler { get; set; } = null!;
+
+    [Inject]
+    public ICategoryHandler CategoryHandler { get; set; } = null!;
     [Inject]
     public NavigationManager NavigationManager { get; set; } = null!;
     [Inject]
     public ISnackbar Snackbar { get; set; } = null!;
+    #endregion
+
+    #region overrides
+    protected override async Task OnInitializedAsync()
+    {
+        IsBusy = true;
+        try
+        {
+            var request = new GetAllCategoriesRequest();
+            var result = await CategoryHandler.GetAllAsync(request);
+
+            if (result.IsSuccess)
+            {
+                Categories = result.Data ?? [];
+                InputModel.CategoryId = Categories.FirstOrDefault()?.Id ?? 0;
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
     #endregion
 
     #region Methods
@@ -28,7 +61,7 @@ public partial class CreateTransactionPage : ComponentBase
 
         try
         {
-            var result = await Handler.CreateAsync(InputModel);
+            var result = await TransactionHandler.CreateAsync(InputModel);
             if (result.IsSuccess)
             {
                 Snackbar.Add(result.Message, Severity.Success);
