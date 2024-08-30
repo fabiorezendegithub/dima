67bf01240020813b469358bb5919f894d105fb24
Fabio Rezende <fabio.rezende@gmail.com>
2024-08-30 13:32:47 -0300

    CRUD: MudDataGrid Transactions
    - Agora comecamos a construir nossa tela de listagem.
    - No razor apenas montamos as configuracoes de nossa datagrid, com
      destaque a opcao nova ColumResizeMode, que permite alterar o tamanho
      da coluna
    - No cs criamos o GetTransactions, que sera o responsavel por pegar as
      transacoes, por ano e mes selecionados, esse sera um metodo privado
    - Tambem no cs criamos o filter de transactions que sera usado na busca
      das informacoes, e bem parecido com o temos na listagem de categorias

--- Dima.Web/Pages/Transactions/List.razor.cs
+++ Dima.Web/Pages/Transactions/List.razor.cs
@@ -1,6 +1,8 @@
-﻿using Dima.Core.Handlers;
+﻿using Dima.Core.Common.Extensions;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
 using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
@@ -32,5 +34,83 @@ public partial class ListTransactionsPage : ComponentBase
     [Inject]
     public IDialogService DialogService { get; set; } = null!;
     #endregion
+
+    #region overrides
+    protected override async Task OnInitializedAsync()
+        => await GetTransactions();
     
+    #endregion
+
+    #region Private Methods
+    public async Task GetTransactions()
+    {
+        IsBusy = true;
+
+        try
+        {
+            var request = new GetTransactionsByPeriodRequest 
+            {
+                StartDate = DateTime.Now.GetFirstDay(CurrentYear, CurrentMonth),
+                EndDate = DateTime.Now.GetLastDay(CurrentYear, CurrentMonth),
+                PageNumber = 1,
+                PageSize = 1000
+            };
+
+            var result = await Handler.GetByPeriodAsync(request);
+
+            if (result.IsSuccess)
+                Transactions = result.Data ?? [];
+
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    public Func<Transaction, bool> Filter => transaction =>
+    {
+        if (string.IsNullOrEmpty(SearchTerm))
+            return true;
+
+        return transaction.Id.ToString().Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)
+            || transaction.Title.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)
+    };
+
+
+    public async void OnDeleteButtonClickedAsync(long id, string title)
+    {
+        var result = await DialogService.ShowMessageBox(
+            "ATENÇÃO",
+            $"Ao prosseguir a categoria {title} será excluída. Essa é uma ação irreversível! Deseja continuar?",
+            yesText: "EXCLUIR",
+            noText: "CANCELAR");
+
+        if (result is true)
+            await OnDeleteAsync(id, title);
+
+        StateHasChanged();
+    }
+
+    public async Task OnDeleteAsync(long id, string title)
+    {
+        try
+        {
+            var request = new DeleteCategoryRequest { Id = id };
+            await Handler.DeleteAsync(request);
+            Categories.RemoveAll(x => x.Id == id);
+            Snackbar.Add($"Categoria {title} excluída com sucesso.", Severity.Success);
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add(ex.Message, Severity.Error);
+        }
+    }
+    #endregion
 }
