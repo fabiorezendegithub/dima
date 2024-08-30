13d5b399e0b753b080addf95aea9b83bd908f837
Fabio Rezende <fabio.rezende@gmail.com>
2024-08-30 13:14:00 -0300

    CRUD: List Transactions
    - Agora começamos a trabalhar no cs da nossa lista.
    - Como diferencial, temos as variaveis ano e mes que utilizaremos
      posteriormente
    - Tambem temos uma variavel de Years, pois nossa pesquisa so ira
      pesquisar o ano atual e mais dois anos para tras, pode ser ajustado
      para o periodo que quiser buscar

---
+++ Dima.Web/Pages/Transactions/List.razor.cs
@@ -0,0 +1,36 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Transactions;
+
+public partial class ListTransactionsPage : ComponentBase
+{
+    #region Properties
+    public bool IsBusy { get; set; } = false;
+    public List<Transaction> Transactions { get; set; } = [];
+    public string SearchTerm { get; set; } = string.Empty;
+    public int CurrentYear { get; set; } = DateTime.Now.Year;
+    public int CurrentMonth { get; set; } = DateTime.Now.Month;
+    public int[] Year { get; set; } =
+    {
+        DateTime.Now.Year,
+        DateTime.Now.AddYears(-1).Year,
+        DateTime.Now.AddYears(-2).Year,
+        DateTime.Now.AddYears(-3).Year
+    };
+    #endregion
+
+    #region Services
+    [Inject]
+    public ITransactionHandler Handler { get; set; } = null!;
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    [Inject]
+    public IDialogService DialogService { get; set; } = null!;
+    #endregion
+    
+}
