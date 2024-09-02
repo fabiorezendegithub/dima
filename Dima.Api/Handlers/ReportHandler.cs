ce98400611545db42efa6f6e5753e5020799d0d4
Fabio Rezende <fabio.rezende@gmail.com>
2024-09-02 00:38:06 -0300

    GRAFICOS: Resumo Financeiro
    - Nessa parte implementamos o resumo financeiro. A particularidade foi o
      truque para o group by. Não havia necessidade de group by mas
      precisamos dele para o sum, por isso foi jogado um x =1 so para fazer
      o group by. essa e uma jogada sempre que tiver esse problema

--- Dima.Api/Handlers/ReportHandler.cs
+++ Dima.Api/Handlers/ReportHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Dima.Api.Data;
+using Dima.Core.Enum;
 using Dima.Core.Handlers;
 using Dima.Core.Models.Reports;
 using Dima.Core.Requests.Reports;
@@ -30,9 +31,30 @@ public class ReportHandler(AppDbContext context) : IReportHandler
         }
     }
 
-    public Task<Response<FinancialSummary?>> GetFinancialSummaryReportAsync(GetFinancialSummaryRequest request)
+    public async Task<Response<FinancialSummary?>> GetFinancialSummaryReportAsync(GetFinancialSummaryRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+            var data = await context
+                              .Transactions
+                              .AsNoTracking()
+                              .Where(x => x.UserId != request.UserId
+                                       && x.PaidOrReceivedAt >= startDate
+                                       && x.PaidOrReceivedAt <= DateTime.Now)
+                              .GroupBy(x => 1)
+                              .Select(x => new FinancialSummary(
+                                  request.UserId,
+                                  x.Where(ty => ty.Type == ETransactionType.Deposit).Sum(t => t.Amount),
+                                  x.Where(ty => ty.Type == ETransactionType.WithDraw).Sum(t => t.Amount)))
+                              .FirstOrDefaultAsync();
+            return new Response<FinancialSummary?>(data);
+        }
+        catch
+        {
+            return new Response<FinancialSummary?>(null, 500, "Não foi possível obter o resultado financeiro");
+        }
     }
 
     public async Task<Response<List<IncomesAndExpenses>?>> GetIncomesAndExpensesReportAsync(GetIncomesAndExpensesRequest request)
