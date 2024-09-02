d918fb42dea88e6986651d15b985fac7d33be8b7
Fabio Rezende <fabio.rezende@gmail.com>
2024-09-01 22:45:03 -0300

    GRAFICOS:  IReportHandler
    - Nessa etapa criamos a interface do report e seus requests
    - Tambem renomeamos um Models que estava com o nome errado para
      ExpensesByCategory

--- Dima.Core/Models/Reports/DispensesByCategory.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Dima.Core.Models.Reports;
-
-public record DispensesByCategory(string UserId, string Category, int Year, decimal Expenses);
---
+++ Dima.Core/Requests/Reports/GetExpensesByCategoryRequest.cs
@@ -0,0 +1,5 @@
+﻿namespace Dima.Core.Requests.Reports;
+
+public class GetExpensesByCategoryRequest : Request
+{
+}
