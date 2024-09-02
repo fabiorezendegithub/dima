eeb4cf5dae45f05d486256c946482329cd06385c
Fabio Rezende <fabio.rezende@gmail.com>
2024-09-01 23:45:09 -0300

    GRAFICOS: Report Handler
    - Desta vez começamos a fazer os handlers da api
    - Ficou pendente apenas um que iremos utilizar o linq mesmo para fazer
      por ser mais simples e não criamos uma view para ele

---
+++ Dima.Api/Handlers/ReportHandler.cs
@@ -0,0 +1,79 @@
+﻿using Dima.Api.Data;
+using Dima.Core.Handlers;
+using Dima.Core.Models.Reports;
+using Dima.Core.Requests.Reports;
+using Dima.Core.Responses;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dima.Api.Handlers;
+
+public class ReportHandler(AppDbContext context) : IReportHandler
+{
+    public async Task<Response<List<ExpensesByCategory>?>> GetExpensesByCategoryReportAsync(GetExpensesByCategoryRequest request)
+    {
+        try
+        {
+            var data = await context
+                         .ExpensesByCategories
+                         .AsNoTracking()
+                         .Where(x => x.UserId == request.UserId)
+                         .OrderByDescending(x => x.Year)
+                         .ThenBy(x => x.Category)
+                         .ToListAsync();
+
+            return new Response<List<ExpensesByCategory>?>(data);
+        }
+        catch
+        {
+            return new Response<List<ExpensesByCategory>?>(null, 500, "Não foi possível obter as entradas por categoria");
+
+        }
+    }
+
+    public Task<Response<FinancialSummary?>> GetFinancialSummaryReportAsync(GetFinancialSummaryRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Response<List<IncomesAndExpenses>?>> GetIncomesAndExpensesReportAsync(GetIncomesAndExpensesRequest request)
+    {
+        try
+        {
+            var data = await context
+                         .IncomesAndExpenses
+                         .AsNoTracking()
+                         .Where(x => x.UserId == request.UserId)
+                         .OrderByDescending(x => x.Year)
+                         .ThenBy(x => x.Month)
+                         .ToListAsync();
+
+            return new Response<List<IncomesAndExpenses>?>(data);
+        }
+        catch
+        {
+            return new Response<List<IncomesAndExpenses>?>(null, 500, "Não foi possível obter as entradas e saídas");
+
+        }        
+    }
+
+    public async Task<Response<List<IncomesByCategory>?>> GetIncomesByCategoryReportAsync(GetIncomesByCategoryRequest request)
+    {
+        try
+        {
+            var data = await context
+                         .IncomesByCategories
+                         .AsNoTracking()
+                         .Where(x => x.UserId == request.UserId)
+                         .OrderByDescending(x => x.Year)
+                         .ThenBy(x => x.Category)
+                         .ToListAsync();
+
+            return new Response<List<IncomesByCategory>?>(data);
+        }
+        catch
+        {
+            return new Response<List<IncomesByCategory>?>(null, 500, "Não foi possível obter as entradas por categoria");
+
+        }
+    }
+}
