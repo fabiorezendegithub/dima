b72e0b6466287f7bdfd4bf36a018411a40f4d173
Fabio Rezende <fabio.rezende@gmail.com>
2024-09-08 16:41:15 -0300

    PEDIDOS: Produto
    - Agora fizemos a classe produto
    - Uma coisa interessante a dizer é que a escolha de titulo e descricao
      são importantes inclusive na busca do google, muitas vezes até mesmo
      adicionamos Summary com esse intuito de melhorar a busca do google.
    - Futuramente iremos cachear o nosso produto, e e importante decidir o
      que cachear. Muitas vezes o produto e muito grande, com mais de 5000
      caracteres, sera que é util cachear isso? talvez sim, talvez não, tudo
      depende do contexto. Preço normalmente não cacheamos, porque apesar de
      ser um item pequeno, ele muda com frequencia, então é bom sempre
      buscá-lo.
    - Algo importante a dizer é que podemos cachear alguns items do produto
      e outros não. Por exemplo, cachear o titulo e a descrição que mudam
      com pouca frequencia, e nao cachear o preço que tem uma mudança
      considerável.

--- Dima.Core/Models/Product.cs
+++ Dima.Core/Models/Product.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dima.Core.Models;
 
-namespace Dima.Core.Models
+public class Product
 {
-    internal class Product
-    {
-    }
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public decimal Price { get; set; }
 }
