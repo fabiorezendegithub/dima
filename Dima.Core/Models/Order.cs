354fd11318cb59346c4cf6be11e98a0269f739a6
Fabio Rezende <fabio.rezende@gmail.com>
2024-09-08 16:55:39 -0300

    PEDIDOS: Pedidos
    - Agora iniciamos a nossa classe Order, de pedidos, que será a classe
      mais complexa
    - Nessa primeira parte não tem muita coisa diferente, apenas adicionamos
      a existencia de um produto, que sempre precisará ter, e um voucher,
      que nem sempre vai ter. O total será um calculo em cima desses dois
      itens
    - Com relação a parte de pagamento em si, iremos utilizar algo mais
      genérico, para que possamos alterar como será feito o pagamento, mas
      essa parte só será demonstrada nas próximas aulas

--- Dima.Core/Models/Order.cs
+++ Dima.Core/Models/Order.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dima.Core.Models;
 
-namespace Dima.Core.Models
+public class Order
 {
-    internal class Order
-    {
-    }
+    public long Id { get; set; }
+    public string Number { get; set; } = Guid.NewGuid().ToString("N")[..8];
+
+    public long ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+
+    public long? VoucherId {  get; set; }
+    public Voucher? Voucher { get; set; }
+    
+    public string UserId { get; set; } = string.Empty;
+
+    public decimal Total
+        => Product.Price - (Voucher?.Amount ?? 0);
 }
