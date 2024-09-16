b3eb5d7327cc83fc964a3b203001b1458f1fdf87
Fabio Rezende <fabio.rezende@gmail.com>
2024-09-16 08:15:56 -0300

    PEDIDOS: Feito endpoint de criação e listagem de pedidos

---
+++ Dima.Api/Endpoints/Orders/CancelOrderEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class CancelOrderEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/{id}/cancel", HandleAsync)
+                      .WithName("Orders: Cancel order")
+                      .WithSummary("Cancelar um Pedido")
+                      .WithDescription("Cancelar um Pedido")
+                      .WithOrder(2)
+                      .Produces<Response<Order?>>();
+
+    private static async Task<IResult> HandleAsync(
+        ClaimsPrincipal user,
+        IOrderHandler handler,
+        long id)
+    {
+        var request = new CancelOrderRequest
+        {
+            Id = id,
+            UserId = user.Identity!.Name ?? string.Empty
+        };
+
+        var result = await handler.CancelAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
---
+++ Dima.Api/Endpoints/Orders/GetAllOrdersEndpoint.cs
@@ -0,0 +1,42 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class GetAllOrdersEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("/", HandleAsync)
+                  .WithName("Orders: Get All")
+                  .WithSummary("Busca todos os pedidos do usuário")
+                  .WithDescription("Busca todos os pedidos do usuário")
+                  .WithOrder(5)
+                  .Produces<PagedResponse<List<Order>?>>();
+
+    private static async Task<IResult> HandleAsync(
+        ClaimsPrincipal user,
+        IOrderHandler handler,
+        [FromQuery] int pageNumber = Configuration.DefaultPageNumber,
+        [FromQuery] int pageSize = Configuration.DefaultPageSize)
+    {
+        var request = new GetAllOrdersRequest
+        {
+            UserId = user.Identity!.Name ?? string.Empty,
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+
+        var result = await handler.GetAllAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
