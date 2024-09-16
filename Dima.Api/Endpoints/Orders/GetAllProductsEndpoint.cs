848a9696e1e93d0069aba45a91f15f69bf13ee96
Fabio Rezende <fabio.rezende@gmail.com>
2024-09-16 08:22:40 -0300

    PEDIDOS: Feito endpoint para a listagem de produtos

---
+++ Dima.Api/Endpoints/Orders/GetAllProductsEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Dima.Api.Endpoints.Orders;
+
+public class GetAllProductsEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("/", HandleAsync)
+                  .WithName("Products: Get All")
+                  .WithSummary("Busca todos os produtos")
+                  .WithDescription("Busca todos os produtos")
+                  .WithOrder(4)
+                  .Produces<PagedResponse<List<Product>?>>();
+
+    private static async Task<IResult> HandleAsync(
+        IProductHandler handler,
+        [FromQuery] int pageNumber = Configuration.DefaultPageNumber,
+        [FromQuery] int pageSize = Configuration.DefaultPageSize)
+    {
+        var request = new GetAllProductsRequest
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+
+        var result = await handler.GetAllAsync(request);
+
+        return result.IsSuccess
+            ? TypedResults.Ok(result)
+            : TypedResults.BadRequest(result);
+    }
+}
