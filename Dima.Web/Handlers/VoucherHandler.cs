c9578b9ae6f9b7f2dc9fa1f2254bba6f32437376
fabio rezende <fabio.rezende@gmail.com>
2024-10-07 08:18:44 -0300

    PEDIDOS-FRONT: Criando e registrando os Handlers
    - Nessa etapa iremos começar a implementar o nosso front end para buscar
      as informações do nosso backend
    - Para isso criamos o VoucherHandler, ProductHandler e OrderHandler
      herdando das interfaces do core
    - Com isso feito, a próxima etapa foi registrar os novos handler dentro
      do Program.cs

---
+++ Dima.Web/Handlers/VoucherHandler.cs
@@ -0,0 +1,14 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Dima.Core.Responses;
+
+namespace Dima.Web.Handlers;
+
+public class VoucherHandler : IVoucherHandler
+{
+    public Task<Response<Voucher?>> GetByNumberAsync(GetVoucherBynumberRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
