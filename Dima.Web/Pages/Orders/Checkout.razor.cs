602c71da2f45f656dd592f541dc0747a1d905e18
fabio rezende <fabio.rezende@gmail.com>
2024-10-21 12:38:45 -0300

    PEDIDOS-FRONTEND: Iniciando pagina Checkout
    - Iniciamos a criação da página checkout.
    - Nesta pagina, temos dois novos items. Primeiro é o parameters, que é
      pego direto da url
    - Segundo e o SupplyParameterFromQuery, que tambem vem da query, mas
      depois do parameter (?voucher)

---
+++ Dima.Web/Pages/Orders/Checkout.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Dima.Web.Pages.Orders;
+
+public partial class CheckoutPage : ComponentBase
+{
+    #region Parameters
+    [Parameter]
+    public string ProductSlug { get; set; } = string.Empty;
+    [SupplyParameterFromQuery(Name ="voucher")]
+    public string? VoucherNumber { get; set; }
+
+    #endregion
+    #region Properties
+    //public bool IsBusy { get; set; } = false;
+    //public CreateCategoryRequest InputModel { get; set; } = new();
+    //#endregion
+
+    //#region Services
+    //[Inject]
+    //public ICategoryHandler Handler { get; set; } = null!;
+    //[Inject]
+    //public NavigationManager NavigationManager { get; set; } = null!;
+    //[Inject]
+    //public ISnackbar Snackbar { get; set; } = null!;
+    #endregion
+
+    #region Methods
+    //public async Task OnValidSubmitAsync()
+    //{
+    //    IsBusy = true;
+
+    //    try
+    //    {
+    //        var result = await Handler.CreateAsync(InputModel);
+    //        if (result.IsSuccess)
+    //        {
+    //            Snackbar.Add(result.Message ?? "Categoria criada com sucesso", Severity.Success);
+    //            NavigationManager.NavigateTo("/categorias");
+    //        }
+    //        else
+    //        {
+    //            Snackbar.Add(result.Message ?? "Erro ao criar categoria", Severity.Error);
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        Snackbar.Add(ex.Message, Severity.Error);
+    //    }
+    //    finally
+    //    {
+    //        IsBusy = false;
+    //    }
+    //}
+    #endregion
+}
