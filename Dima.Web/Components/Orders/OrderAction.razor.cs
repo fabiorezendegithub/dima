5e29482d42aeb861671eb2d74b009b4e2b290d27
fabio rezende <fabio.rezende@gmail.com>
2024-11-11 08:26:02 -0300

    PEDIDOS-FRONT: Adiciona metodo cancela pedido
    - Foi criado o metodo no cs responsável por cancelar um pedido. Ainda
      está pendente a questão de atualizar a página, pois mesmo cancelando o
      pedido no banco, esta atualização não está sendo informada na página.
    - Iremos tratar essa questão no próximo commit

--- Dima.Web/Components/Orders/OrderAction.razor.cs
+++ Dima.Web/Components/Orders/OrderAction.razor.cs
@@ -1,15 +1,58 @@
+using Dima.Core.Handlers;
 using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Components.Orders;
 
 public partial class OrderActionComponent : ComponentBase
 {
     #region Parameters
-    
-    [Parameter, EditorRequired]
-    public Order Order { get; set; } = null!;
+
+    [Parameter, EditorRequired] public Order Order { get; set; } = null!;
 
     #endregion
 
+    #region Services
+
+    [Inject] public IDialogService DialogService { get; set; } = null!;
+    [Inject] public IOrderHandler OrderHandler { get; set; } = null!;
+    [Inject] public ISnackbar Snackbar { get; set; } = null!;
+
+    #endregion
+
+    #region Public Methods
+
+    public async void OnCancelButtonClicked()
+    {
+        var result = await DialogService.ShowMessageBox(
+            "ATENÇÃO",
+            "Deseja realmente cancelar este pedido?",
+            yesText: "SIM", cancelText: "NÃO");
+
+        if (result is not null && result == true)
+            await CancelOrderAsync();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private async Task CancelOrderAsync()
+    {
+        var request = new CancelOrderRequest
+        {
+            Id = Order.Id,
+        };
+
+        var result = await OrderHandler.CancelAsync(request);
+
+        if (result.IsSuccess)
+            Snackbar.Add(result.Message ?? string.Empty, Severity.Success);
+        else
+            Snackbar.Add(result.Message ?? string.Empty, Severity.Error);
+    }
+
+    #endregion
 }
